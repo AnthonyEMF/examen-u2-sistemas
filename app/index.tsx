f007c45f3ca50158293bd99bd82bce4851fe3181
AnthonyEMF <anthony07miranda@gmail.com>
2025-03-13 10:15:36 -0600

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View>
+      <Text className='text-3xl text-center mt-20 font-bold'>Examen II - Anthony Miranda</Text>
+    </View>
+  )
+}
+
+export default Index
